d637dad93ee373fccbababc75db74504561b42f1
spvedantham <125249085+spvedantham@users.noreply.github.com>
2023-02-13 19:57:39 +0530

    Update streamlit_app.py
    Fixed the spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast-Menu')
 streamlit.text('Omega 3 Blue berry Oatmel')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamit.text('Hard boiled Free-Range Egg')
+streamlit.text('Hard boiled Free-Range Egg')
 
